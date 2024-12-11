720a9f0b836c7d53f341ecc5bd912e6bf04f9d2f
Alexa Xu <xby04152002@163.com>
2024-12-10 20:14:10 -0500

    encrypt and decrypt parts

---
+++ user_system/encrypt_decrypt.py
@@ -0,0 +1,72 @@
+import base64
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+
+def encrypt_message(message, public_key):
+    '''
+    This function encrypts a message using the public key provided.
+    The message is first encoded to bytes, then encrypted using the public key.
+
+    ***Parameters***
+    message: str
+        The message to be encrypted.
+    public_key: RSAPublicKey
+        The public key to be used for encryption.
+
+    ***Returns***
+    str
+        The encrypted message in base64 format.
+    '''
+    encrypted_message = public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return base64.b64encode(encrypted_message).decode('utf-8')
+
+
+def decrypt_message(encrypted_message, private_key):
+    '''
+    This function decrypts a message using the private key provided.
+    The encrypted message is first decoded from base64, then decrypted using the private key.
+
+    ***Parameters***
+    encrypted_message: str
+        The encrypted message in base64 format.
+    private_key: RSAPrivateKey
+
+    ***Returns***
+    str
+        The decrypted message.
+    '''
+    encrypted_message_bytes = base64.b64decode(encrypted_message.encode('utf-8'))
+    decrypted_message = private_key.decrypt(
+        encrypted_message_bytes,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted_message.decode('utf-8')
+
+
+def load_keys():
+    '''
+    This function loads the private and public keys from the files.
+
+    ***Returns***
+    RSAPrivateKey, RSAPublicKey
+        The private and public keys loaded from the files.
+    '''
+    with open("user_info_data/private_key.pem", "rb") as f:
+        private_key = serialization.load_pem_private_key(f.read(), password=None)
+
+    with open("user_info_data/public_key.pem", "rb") as f:
+        public_key = serialization.load_pem_public_key(f.read())
+
+    return private_key, public_key
---
+++ user_system/generate_keys.py
@@ -0,0 +1,32 @@
+import base64
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+'''
+This script generates a private and public key pair and saves them to the current directory.
+When generating a key pair, the public key is derived from the private key.
+ATTENTION: This script should ONLY be run ONCE to generate and save the key pair.
+'''
+
+def generate_and_save_keys():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = private_key.public_key()
+
+    with open("private_key.pem", "wb") as f:
+        f.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+
+    with open("public_key.pem", "wb") as f:
+        f.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+
+
+generate_and_save_keys()  
