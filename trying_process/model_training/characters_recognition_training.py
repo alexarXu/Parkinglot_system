57ec5a7e1d30de47b5fce61253440cb58d89ff6c
Alexa Xu <xby04152002@163.com>
2024-12-10 20:18:29 -0500

    try recognition model

---
+++ trying_process/model_training/characters_recognition_training.py
@@ -0,0 +1,108 @@
+'''================================================================================================
+This script is a trying process to train a model to recognize characters from license plates.
+
+The dataset used in this process is in the dataset folder. The dataset consists of 36 classes,
+which are the 26 letters of the alphabet and the 10 digits.
+
+The model is a simple convolutional neural network (CNN) with three convolutional layers and two
+fully connected layers. The output layer has 36 units, one for each class.
+
+This model works well for recognizing characters. The weights of the trained model are saved in
+the my_model.h5 file. You can run this script to retrain the model.
+==============================================================================================='''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential
+
+
+data_dir = r'C:\Users\administer\Downloads\ALPR_IndonesiaPlateNumber_ComputerVision-main\dataset' # training dataset folder
+
+batch_size = 32
+img_height = 40
+img_width = 40
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+
+num_classes = 36
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs=15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(14, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+model.save('my_model.h5') # Save trained model
